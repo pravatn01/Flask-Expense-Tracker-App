cacdc059be9db302afda86d5b75ff4f859901c64
Pravat Nagarkoti <prbt333@gmail.com>
2024-12-19 18:15:53 +0545

    improvements in ui and finalizing project

--- app.py
+++ app.py
@@ -1,38 +1,42 @@
 from flask import Flask, request, render_template, redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
+local_tz = pytz.timezone('Asia/Kathmandu')
 
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expense.db'
 app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Expense(db.Model):
-    sn = db.Column(db.Integer, primary_key = True)
-    category = db.Column(db.String(50), nullable = False)
-    desc = db.Column(db.String(100), nullable = False)
-    amount = db.Column(db.Float, nullable = False)
-    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    sn = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(50), nullable=False)
+    desc = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.now(pytz.utc))  # Use pytz.utc
 
     def __init__(self, category, desc, amount):
         self.category = category
         self.desc = desc
         self.amount = float(amount)
 
-
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        expense = Expense(request.form['category'], request.form['desc'],request.form['amount'])
+        expense = Expense(request.form['category'], request.form['desc'], request.form['amount'])
         db.session.add(expense)
         db.session.commit()
         return redirect(url_for('index'))
 
     allexpense = Expense.query.all()
+
     for expense in allexpense:
-        expense.date_added = expense.date_added.strftime('%Y-%m-%d | %H:%M')
-    return render_template('index.html', allexpense = allexpense)
+        expense.date_added = expense.date_added.replace(tzinfo=pytz.utc).astimezone(local_tz)
+
+    return render_template('index.html', allexpense=allexpense)
+
 
 @app.route('/edit/<int:var>', methods=['GET','POST'])
 def edit(var):
@@ -48,7 +52,6 @@ def edit(var):
     expense = Expense.query.filter_by(sn = var).first()
     return render_template('edit.html', expense = expense)
 
-
 @app.route('/delete/<int:var>', methods=['GET', 'POST'])
 def delete(var):
     expense = Expense.query.filter_by(sn=var).first()
@@ -56,7 +59,6 @@ def delete(var):
         db.session.delete(expense)
         db.session.commit()
     return redirect('/')
-
 
 if __name__ == '__main__':
     app.run(debug = True)
